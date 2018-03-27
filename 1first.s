fb2aa1e755db3ec9b0befe18b23f18bd63d11e46
mgh3326 <mgh3326@naver.com>
2018-03-27 14:18:39 +0900

    update

---
+++ 1first.s
@@ -0,0 +1,23 @@
+#
+#  First MIPS program
+#
+          .data		
+One:   .word    1          # first value, initialized to 1
+Two:   .word    2          # second value, initialized to 2
+
+# program to add 1+2!
+
+          .text
+main:		
+# load constants and add values
+          lw       $s0,One        # first operand
+          lw       $s1,Two       # second operand
+          add     $s2,$s0,$s1   # add
+# note registers in an add instrcution need not be distinct
+# print value
+          move   $a0,$s2       # pass result to syscall
+          li          $v0, 1        # specify syscall is a write integer
+          syscall                   # execute syscall
+# all done....
+          li          $v0, 10       #  adios.....
+          syscall	
---
+++ pow2.s
@@ -0,0 +1,28 @@
+          	.data		
+Two:   	.word    	2          # first value, initialized to 2
+Three: 	.word    	3          # second value, initialized to 3
+pow2:	.word	1, 2, 4, 8, 16, 32, 64, 128        
+	.text		
+main:		
+# load constants and add values
+          	lw       	$a0,Two        	# first operand	$a0 = 2 (b)
+         	lw       	$a1,Three       	# second operand	$a1 = 3 (c)		
+	addu	$a0,$a0,$a1          # a = b + c, $a0 = a
+	bltz	$a0, Exceed          	# goto Exceed if $v0 < 0
+	slti	$v0,$a0,8              # $v0 = a < 8
+	beq	$v0,$zero, Exceed  # goto Exceed if $v0 == 0
+	la	$v1,pow2		# $v1 = pow2 address
+	sll	$v0,$a0,2	# $v0 = a*4
+	addu	$v0,$v0,$v1	# $v0 = pow2 + a*4
+	lw	$v0,0($v0)	# $v0 = pow2[a]
+	j	Return		# goto Return
+Exceed:	addu	$v0,$zero,$zero	# $v0 = 0
+Return:
+# print value
+          	move   	$a0,$v0       	# pass result to syscall
+          	li          	$v0, 1        	# specify syscall is a write integer
+          	syscall                   	# execute syscall
+# all done....
+         	li          	$v0, 10       	#  adios.....
+         	syscall	
+
